489ea9e25818d821a90ee17a1175de1b532e8586
Sophie Roe <sophie.roe@Nology.local>
2020-11-15 19:17:55 +0000

    updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import styles from "./App.module.scss";
+import iceland from "./iceland.jpg"
 
 const App = () => {
   // Store our data/collections here for us to show
-  const [data, setData] = useState([]); 
+  const [data, setData] = useState([]);
   // Store the name of the person we are going to create
   // NOTE: this is the value in the form input
-  const [input, setInput] = useState("");
+  const [country, setCountry] = useState("")
+  const [sights, setSights] = useState("")
   useEffect(() => {
     // 1. On component load/mount let's make a call to our API and show some data on the page....
     handleFetch();
@@ -26,6 +28,7 @@ const App = () => {
   const handleSubmit = (e) => {
     // Stop our page submitting
     e.preventDefault();
+
     // *********************************
     // 3. TODO!! Let's make POST request to our API...
     // *********************************
@@ -35,16 +38,14 @@ const App = () => {
       // We are gonna send JSON
       headers: { 'Content-Type': 'application/json' },
       // Data goes in the body
-      body: JSON.stringify({"name": input})
+      body: JSON.stringify({ "country": country, "sights": sights, })
     }
     fetch('http://localhost:8081/create', fetchOptions)
       .then(res => res.json())
       .then(res => {
         console.log("YAY WE GOT OUR RESPONSE BACK....")
         handleFetch();
-        
       })
-
   }
 
   const handleDelete = (doc) => {
@@ -56,26 +57,30 @@ const App = () => {
       // Data goes in the body
       body: JSON.stringify(doc)
     }
-  // const fetchOptions = { method: 'DELETE'}
-  fetch('http://localhost:8081/delete', fetchOptions)
+    // const fetchOptions = { method: 'DELETE'}
+    fetch('http://localhost:8081/delete', fetchOptions)
       .then(() => {
         console.log("sucessfully deleted" + doc.name)
         handleFetch();
       })
-    }
+  }
   return (
-    <div className={styles.App}>
-      <h1>To Do List....</h1>
+    <div className={styles.page}>
+      <h1 >Travel Destinations</h1>
       {/* We'll use this later once we have some information in our database
-          to show on the page...... */}  
-      {data.map(data =>(<p>{data.name} <button onClick={() => handleDelete(data)}>Done!</button></p>))}
+          to show on the page...... */}
+      {data.map(data => (<div><p>Country: {data.country}</p><p>Sights: {data.sights}</p> <button onClick={() => handleDelete(data)}>Done!</button></div>))}
       <form className={styles.form}>
-        <label>To do:</label>
+        <h2>Add your dream travel destination!</h2>
         {/* We need to keep track of this textbox so we know what to send */}
-        <input type="text" onChange={(e) => setInput(e.target.value)}/>
+        <div><label>Country: </label>
+        <input type="text" id="country" onChange={(e) => setCountry(e.target.value)} /></div>
+        <div><label>Sights: </label>
+        <input type="text" id="sights" onChange={(e) => setSights(e.target.value)} /></div>
         {/* ON CLICK - we need to send some information to the API */}
         <button onClick={handleSubmit}>Add</button>
       </form>
+      <img src={iceland} alt=""/>
     </div>
   );
 }
